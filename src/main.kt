8498f3fde40766978c423f32e739338079cf2bd2
Muyoba Lutlas <lutakachihota@gmail.com>
2020-10-03 22:12:00 +0200

    lambda's amazing

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,17 @@
 fun main() {
     val addInts = { x: Int, y: Int -> x + y }
-    val result = addInts.invoke(6, 7)
+    val addFive:(Int)->Int = {it+5}
+    val result = addInts.invoke(6, -7)
     val answer = addInts(5,0)
+    val greeting = {"Hello World"}
+    val other = {z:Int->"The value is $z"}
     println(result)
     println(answer)
+    println(greeting())
+    println(other(20))
+    println(
+            addFive(
+                    addInts(6,result)
+            )
+    )
 }
